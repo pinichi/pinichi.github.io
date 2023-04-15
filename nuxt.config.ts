7f92bc370e16a6197c1d8c8c47a0ac8140e4befd
Pinichi Cho <pinichi@hanmail.net>
2023-04-15 18:37:22 +0900

    :tada: Nuxt3, tailwindCSS setup

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss"],
+});
+
+// export default {
+//   css: ["~/assets/css/tailwind.css"],
+// };
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-sky-400">pages/index.vue {{ hello }}</div>
+</template>
+
+<script setup>
+const hello = "hello";
+</script>
