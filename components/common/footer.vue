2b97f130207eb54ee9f1107802fc405813adea8f
Pinichi Cho <pinichi@hanmail.net>
2023-04-16 19:31:58 +0900

    :lipstick: DefaultLayout 지정 및 Header, Footer 설정
    default layout 설계 및 Header Footer 구현

---
+++ components/common/footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="max-w-[1440px] mx-auto flex flex-col text-center">
+    © Pinichi 2023
+  </footer>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full h-full">
+    <CommonHeader />
+    <div class="max-w-[1440px] mx-auto h-full px-4 sm:px-6 md:px-8 py-1">
+      <slot />
+    </div>
+    <CommonFooter />
+  </div>
+</template>
+<!--absolute top-0 bottom-0  -->
