3565c4e4305ca2bf9ef517b056d84bdc13ed34a6
Pinichi Cho <pinichi@hanmail.net>
2023-04-17 20:02:09 +0900

    :lipstick: ArticleCard, default layout 디자인 수정

--- components/article/ArticleCard.vue
+++ components/article/ArticleCard.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="w-[350px] cursor-pointer flex flex-col">
+  <nuxt-link class="w-[350px] cursor-pointer flex flex-col" :to="slugUrl">
     <img :src="props.image" class="rounded-xl" />
     <div class="flex gap-x-4 mt-2">
       <div>2020년 3월 12일</div>
       <category-chip :category="props.category" />
     </div>
-    <div class="text-3xl mt-2">{{ props.title }}</div>
-    <div class="text-xl text-gray-400">{{ props.description }}</div>
-  </div>
+    <div class="text-xl mt-2">{{ props.title }}</div>
+    <div class="text-md text-gray-400">{{ props.description }}</div>
+  </nuxt-link>
 </template>
 
 <script setup>
 import CategoryChip from "./CategoryChip.vue";
+const slugUrl = `${props.slug}`;
 
 const props = defineProps({
   image: String,
   title: String,
   description: String,
   category: String,
+  slug: String,
 });
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="w-full h-full">
     <CommonHeader />
-    <div class="max-w-[1440px] mx-auto h-full px-4 sm:px-6 md:px-8 py-1">
+    <div
+      class="max-w-[1440px] mx-auto h-full px-4 sm:px-6 md:px-8 py-4 sm:py-6 md:py-8"
+    >
       <slot />
     </div>
     <CommonFooter />
