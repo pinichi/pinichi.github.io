52c511659f2ef1510a2c4a934ec9ee1b83c710c0
Pinichi Cho <pinichi@hanmail.net>
2023-04-17 19:58:55 +0900

    :necktie: category별 라우팅 페이지 구현
    - 카테고리별 라우팅을 위해 Navigation Component 구현
    - 헤더에 메인페이지 라우팅 추가
    - index페이지 카테고리별 아이템 렌더를 위해 수정

---
+++ components/base/Navigation.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="w-full px-4">
+    <div class="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
+      <Disclosure :defaultOpen="true" v-slot="{ open }">
+        <DisclosureButton
+          class="flex w-full justify-between rounded-lg bg-lime-100 px-4 py-2 text-left text-sm font-medium text-lime-900 hover:bg-lime-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+        >
+          <span>카테고리</span>
+        </DisclosureButton>
+        <template v-if="open">
+          <DisclosurePanel
+            v-for="name in categoryNameList"
+            :key="name"
+            class="flex flex-col px-4 pt-4 pb-2 text-sm"
+            :class="
+              props.category === name ? 'text-green-500' : 'text-gray-500'
+            "
+          >
+            <nuxt-link :key="name" :to="{ query: { category: `${name}` } }">{{
+              name
+            }}</nuxt-link>
+          </DisclosurePanel>
+        </template>
+      </Disclosure>
+      <!-- <Disclosure as="div" class="mt-2" v-slot="{ open }">
+        <DisclosureButton
+          class="flex w-full justify-between rounded-lg bg-green-100 px-4 py-2 text-left text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+        >
+          <span>태그</span>
+          <ChevronUpIcon
+            :class="open ? 'rotate-180 transform' : ''"
+            class="h-5 w-5 text-purple-500"
+          />
+          <div>{{ open }}</div>
+        </DisclosureButton>
+        <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
+          No.
+        </DisclosurePanel>
+      </Disclosure> -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+
+const props = defineProps({
+  category: String,
+  tag: String,
+});
+
+const categoryNameList = ["tech", "design", "daily"];
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="flex">
-    <div class="hidden lg:block min-w-[200px]">
-      <div>카테고리별 필터</div>
-      <div class="border-l border-slate-100">Tech</div>
-      <div class="border-l border-slate-100">Design</div>
-      <div class="border-l border-slate-100">Daily</div>
+    <div class="hidden w-64 shrink-0 lg:block lg:pr-8">
+      <navigation :category="category" />
     </div>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
       <template v-for="article in articleList" :key="article._path">
@@ -13,6 +10,7 @@
           :description="article.description"
           :category="article.category"
           :image="article.coverImage"
+          :slug="article.slug"
         />
       </template>
     </div>
@@ -21,25 +19,55 @@
 
 <script setup>
 import ArticleCard from "../components/article/ArticleCard.vue";
+import Navigation from "../components/base/Navigation.vue";
 
-const { data: articleList } = await useAsyncData("post", async () => {
-  try {
-    const result = await queryContent("/article")
-      .where({ _path: { $ne: "/article" } }) // exclude content/article/index.md
-      .sort({ createdAt: 1 })
-      .only([
-        "title",
-        "image",
-        "_path",
-        "description",
-        "category",
-        "coverImage",
-      ])
-      .find();
-    return result;
-  } catch (err) {
-    router.replace({ path: "/404" });
-    throw createError({ statusCode: 404, statusMessage: "404" });
+const route = useRoute();
+const router = useRouter();
+
+const isCategoryQuery = computed(
+  () =>
+    !!Object.keys(route.query).length &&
+    Object.keys(route.query).includes("category")
+);
+const category = ref(route.query.category ? route.query.category : "");
+watch(
+  () => route.query,
+  () => {
+    category.value = route.query.category ?? "";
   }
+);
+watch(category, () => {
+  router.push({ path: "/", query: { category: category.value } });
 });
+
+const { data: articleList } = await useAsyncData(
+  async () => {
+    try {
+      const articleList = await queryContent("/article")
+        .where({
+          _path: { $ne: "/article" },
+          ...(isCategoryQuery.value
+            ? { category: { $eq: category.value } }
+            : {}),
+        }) // exclude content/article/index.md
+        .sort({ createdAt: 1 })
+        .only([
+          "title",
+          "image",
+          "_path",
+          "description",
+          "category",
+          "coverImage",
+          "slug",
+        ])
+        .find();
+      return articleList;
+    } catch (err) {
+      console.log("err: ", err);
+      router.replace({ path: "/404" });
+      throw createError({ statusCode: 404, statusMessage: "404" });
+    }
+  },
+  { watch: [category] }
+);
 </script>
