696d5c6317640889de02416fd6911635c982d012
Pinichi Cho <pinichi@hanmail.net>
2023-04-19 19:31:28 +0900

    :necktie: pagination 구현
    - @nuxt/contents 의 skip, limit을 이용한 cursorPagination 적용
    - useAsyncData를 사용할때 data의 변화한 값을 사용하고 싶을 경우 useLazyAsyncData를 이용하면 됨
    ref.https://stackoverflow.com/questions/70464990/nuxt3-useasyncdata-not-working-onmounted-lifecycle-hook#comment133099863_70498403
    - IntersectionObserver를 이용하여 infiniteScroll 구현

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="flex">
-    <div class="hidden w-64 shrink-0 lg:block lg:pr-8">
-      <navigation :category="category" />
+  <div class="flex flex-col">
+    <div class="flex">
+      <div class="hidden w-64 shrink-0 lg:block lg:pr-8">
+        <navigation :category="category" />
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <template v-for="article in articleList" :key="article._path">
+          <article-card
+            :title="article.title"
+            :description="article.description"
+            :category="article.category"
+            :image="article.coverImage"
+            :slug="article.slug"
+            :createdAt="article.createdAt"
+          />
+        </template>
+      </div>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-      <template v-for="article in articleList" :key="article._path">
-        <article-card
-          :title="article.title"
-          :description="article.description"
-          :category="article.category"
-          :image="article.coverImage"
-          :slug="article.slug"
-          :createdAt="article.createdAt"
-        />
-      </template>
-    </div>
+    <div ref="indexEl" class="w-full h-1" />
   </div>
 </template>
 
@@ -35,6 +38,8 @@ const category = ref(route.query.category ? route.query.category : "");
 watch(
   () => route.query,
   () => {
+    pageNum.value = 1;
+    isEndofPage.value = false;
     category.value = route.query.category ?? "";
   }
 );
@@ -45,16 +50,22 @@ watch(category, () => {
   });
 });
 
-const { data: articleList } = await useAsyncData(
+const pageNum = ref(1);
+const FETCH_NUM = 12;
+
+const { data } = await useLazyAsyncData(
   async () => {
     try {
-      const articleList = await queryContent("/article")
+      const articeList = await queryContent("/article")
         .where({
           _path: { $ne: "/article" },
+          isPublished: { $ne: false },
           ...(isCategoryQuery.value
             ? { category: { $eq: category.value } }
             : {}),
-        }) // exclude content/article/index.md
+        })
+        .skip(FETCH_NUM * (pageNum.value - 1))
+        .limit(FETCH_NUM)
         .sort({ createdAt: 1 })
         .only([
           "coverImage",
@@ -66,13 +77,60 @@ const { data: articleList } = await useAsyncData(
           "slug",
         ])
         .find();
-      return articleList;
+      return articeList;
     } catch (err) {
       console.log("err: ", err);
       router.replace({ path: "/404" });
       throw createError({ statusCode: 404, statusMessage: "404" });
     }
   },
-  { watch: [category] }
+  { watch: [category, pageNum] }
 );
+
+const articleList = ref([]);
+articleList.value = data.value;
+
+const isEndofPage = ref(false);
+
+watch(
+  data,
+  (newData, oldData) => {
+    if (newData.length === 0 && oldData.length > 0) {
+      isEndofPage.value = true;
+    }
+  },
+  { deep: true }
+);
+
+watch(
+  data,
+  (newData) => {
+    if (!isEndofPage.value && pageNum.value > 1) {
+      articleList.value.push(...newData);
+    } else if (!isEndofPage.value && pageNum.value === 1) {
+      articleList.value = newData;
+    }
+  },
+  { deep: true }
+);
+
+const observer = ref(null);
+const indexEl = ref(null);
+
+onMounted(() => {
+  observer.value = new IntersectionObserver(
+    ([entry]) => {
+      if (entry?.isIntersecting) {
+        console.log("pageNum call: ", pageNum.value);
+        pageNum.value += 1;
+      }
+    },
+    { threshold: 0.3 }
+  );
+  observer.value.observe(indexEl.value);
+});
+
+onUnmounted(() => {
+  observer.value.disconnect();
+});
 </script>
