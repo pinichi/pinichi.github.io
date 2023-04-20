5ecf06d79f2dbfdb32aba277520da384a8ecbb09
Pinichi Cho <pinichi@hanmail.net>
2023-04-21 01:19:03 +0900

    :necktie: Mobile에서 Navigation 대신 상단에 메뉴 추가
    - MobileMenu component 작성
    - Header에 MobileMenu 적용
    - HeadlessUI의 버그로 MenuItem에서 NuxtLink에 click event가 on 되지 않는 문제가 있어 mouse Event로 바인딩
    - ref.https://github.com/tailwindlabs/headlessui/issues/2243#issuecomment-1419106615

---
+++ components/base/MobileMenu.vue
@@ -0,0 +1,76 @@
+<template>
+  <Menu as="div" class="relative inline-block">
+    <MenuButton class="inline-flex w-full pa-4">
+      <Icon name="ic:outline-menu" size="32px" />
+    </MenuButton>
+    <transition
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="transform scale-95 opacity-0"
+      enter-to-class="transform scale-100 opacity-100"
+      leave-active-class="transition duration-75 ease-in"
+      leave-from-class="transform scale-100 opacity-100"
+      leave-to-class="transform scale-95 opacity-0"
+    >
+      <MenuItems
+        class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+      >
+        <div class="py-1">
+          <div class="text-left font-medium p-2">카테고리</div>
+          <MenuItem v-slot="{ active, close }">
+            <nuxt-link
+              :class="[
+                active ? 'bg-lime-100' : '',
+                'flex w-full items-center p-2 text-sm',
+              ]"
+              :to="{ path: '/', query: { category: 'tech' } }"
+              @mouseup="close"
+            >
+              tech
+            </nuxt-link>
+          </MenuItem>
+          <MenuItem v-slot="{ active, close }">
+            <nuxt-link
+              :class="[
+                active ? 'hover:bg-lime-100' : '',
+                'flex w-full items-center p-2 text-sm',
+              ]"
+              @mouseup="close"
+              :to="{ path: '/', query: { category: 'design' } }"
+            >
+              design
+            </nuxt-link>
+          </MenuItem>
+          <MenuItem v-slot="{ active, close }">
+            <nuxt-link
+              :class="[
+                active ? 'hover:bg-lime-100' : '',
+                'flex w-full items-center p-2 text-sm',
+              ]"
+              :to="{ path: '/', query: { category: 'daily' } }"
+              @mouseup="close"
+            >
+              daily
+            </nuxt-link>
+          </MenuItem>
+        </div>
+        <div class="pt-1">
+          <MenuItem v-slot="{ active, close }">
+            <nuxt-link
+              :class="[
+                active ? 'hover:bg-lime-100' : '',
+                'flex w-full items-center p-2 rounded-b-md',
+              ]"
+              @mouseup="close"
+            >
+              About
+            </nuxt-link>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
+
+<script setup>
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+</script>
--- components/common/header.vue
+++ components/common/header.vue
@@ -1,16 +1,22 @@
 <template>
   <header class="border-b sticky top-0 bg-white">
-    <div class="max-w-[1440px] mx-auto flex h-14 px-4 sm:px-6 md:px-8 py-1">
+    <div
+      class="flex items-center max-w-[1440px] mx-auto h-14 px-4 sm:px-6 md:px-8 py-1"
+    >
       <nuxt-link
         :to="'/'"
         class="flex items-center flex-grow text-lime-800 text-2xl"
       >
         Pinichi's Blog
       </nuxt-link>
-      <div class="flex items-center gap-x-4">
+      <div class="lg:flex hidden items-center gap-x-4">
         <div>About</div>
         <div>Mode</div>
       </div>
+      <mobile-menu class="lg:hidden" />
     </div>
   </header>
 </template>
+<script setup>
+import MobileMenu from "../base/MobileMenu.vue";
+</script>
